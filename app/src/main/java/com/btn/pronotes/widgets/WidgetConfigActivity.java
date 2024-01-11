cf4fa17c3edb91b02e8b9730142a720e50b0d555
Brett Hudson <Brett@brett-techrepair.com>
2024-01-10 22:40:40 -0500

    Revert "Widget bug"
    This reverts commit 4058fc6b0d72211b9eb965a779ae011a263dba9b.

--- app/src/main/java/com/btn/pronotes/widgets/WidgetConfigActivity.java
+++ app/src/main/java/com/btn/pronotes/widgets/WidgetConfigActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -223,7 +222,6 @@ public class WidgetConfigActivity extends Activity {
             @Override
             public void onNoteClick(Notes note) {
                 updateWidgetWithNote(note);
-                String formattedNoteDescription = Html.fromHtml(note.getNotes()).toString();
 
                 // Dismiss the dialog
                 dialog.dismiss();
